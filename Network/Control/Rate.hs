79b4172e643fe9d652a6e8918caf5011957e497c
Kazu Yamamoto <kazu@iij.ad.jp>
2023-11-15 13:20:49 +0900

    doc

--- Network/Control/Rate.hs
+++ Network/Control/Rate.hs
@@ -1,4 +1,5 @@
 module Network.Control.Rate (
+    -- * Rate control
     Rate,
     newRate,
     getRate,
@@ -7,15 +8,20 @@ module Network.Control.Rate (
 import Data.IORef
 import Data.UnixTime
 
+-- | Type for rating.
 newtype Rate = Rate (IORef Counter)
 
 data Counter = Counter Int UnixTime
 
+-- | Creating a new 'Rate'.
 newRate :: IO Rate
 newRate = do
     cntr <- Counter 0 <$> getUnixTime
     Rate <$> newIORef cntr
 
+-- | Getting the current rate.
+-- If one or more seconds have passed since the previous call, 1 is
+--   returned. Otherwise, incremented counter number is returned.
 getRate :: Rate -> IO Int
 getRate (Rate ref) = do
     Counter n beg <- readIORef ref
