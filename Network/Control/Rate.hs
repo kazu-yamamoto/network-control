26d93c00e8aae2a35243c817eecea71f28cfb6d9
Kazu Yamamoto <kazu@iij.ad.jp>
2023-11-15 14:03:18 +0900

    implementing addRate

--- Network/Control/Rate.hs
+++ Network/Control/Rate.hs
@@ -3,6 +3,7 @@ module Network.Control.Rate (
     Rate,
     newRate,
     getRate,
+    addRate,
 ) where
 
 import Data.IORef
@@ -20,18 +21,26 @@ newRate = do
     Rate <$> newIORef cntr
 
 -- | Getting the current rate.
--- If one or more seconds have passed since the previous call, 1 is
---   returned. Otherwise, incremented counter number is returned.
+-- If one or more seconds have passed since the previous call, the
+-- counter is re-initialized with 1 and it is returned.  Otherwise,
+-- incremented counter number is returned.
 getRate :: Rate -> IO Int
-getRate (Rate ref) = do
+getRate r = addRate r 1
+
+-- | Getting the current rate.
+-- If one or more seconds have passed since the previous call, the
+-- counter is re-initialized with the second argument and it is
+-- returned.  Otherwise, increased counter number is returned.
+addRate :: Rate -> Int -> IO Int
+addRate (Rate ref) x = do
     Counter n beg <- readIORef ref
     cur <- getUnixTime
     if (cur `diffUnixTime` beg) > 1
         then do
-            let n' = 1
+            let n' = x
             writeIORef ref $ Counter n' cur
             return n'
         else do
-            let n' = n + 1
+            let n' = n + x
             writeIORef ref $ Counter n' beg
             return n'
